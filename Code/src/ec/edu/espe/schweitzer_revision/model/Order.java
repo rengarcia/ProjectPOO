5bf223a14e5cd75a52f57b43e518d596001fe53f
jhonyagus <jhonyagus@hotmail.com>
2019-04-26 19:39:23 -0500

    Swetchweitzer_revisionSystem_1.0

---
+++ Code/src/ec/edu/espe/schweitzer_revision/model/Order.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.schweitzer_revision.model;
+
+/**
+ *
+ * @author User
+ */
+public class Order {
+    private String date;
+    private String address;
+    private int id;
+    private String type;
+    private boolean priority;
+    private String description;
+
+  
+    
+    public void generateID(){
+        // to do
+    }
+    
+    public void setPriority(){
+        // to do
+    }
+    
+}
