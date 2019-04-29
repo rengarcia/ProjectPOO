eb8afb318dc0d63278b0fca7ecad8b07571d7ad4
jhonyagus <jhonyagus@hotmail.com>
2019-04-28 20:07:56 -0500

    UpdateSparePartAndTechnician

--- Code/Schweitzer_revision_System 1.1/src/ec/edu/espe/schweitzer_revision/model/SparePart.java
+++ Code/Schweitzer_revision_System 1.1/src/ec/edu/espe/schweitzer_revision/model/SparePart.java
@@ -7,7 +7,7 @@ package ec.edu.espe.schweitzer_revision.model;
 
 /**
  *
- * @author User
+ * @author Jhony Naranjo
  */
 public class SparePart {
     private String name;
@@ -15,13 +15,50 @@ public class SparePart {
     private String id;
     private int quantity;
 
-  
-    
+	public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public SparePart(String name, float price, String id, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.id = id;
+        this.quantity = quantity;
+    }
+
     public void updateQuantity(){
-        // to do
+       this.quantity--;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
     
     public String listItem(){
-        return "aa";
+        return "El producto "+ this.name +" tiene el valor de "+ this.price+ " con el id: "+ this.id+ " y se encuentra en stock: "+ this.quantity;
     }
 }
