f2a988edd68619baf4d1a9f13fee764cc3454c53
andro1010ag <andresgarcia17@icloud.com>
2019-06-05 23:59:58 -0500

    Structure of tests

---
+++ 06.Code/SchweitzerSystem_2.0/test/ec/edu/espe/schweitzer_revision/model/PasswordTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.schweitzer_revision.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Andres Garcia
+ */
+public class PasswordTest {
+    
+    public PasswordTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        fail("The test case is a prototype.");
+    }
+    
+}
