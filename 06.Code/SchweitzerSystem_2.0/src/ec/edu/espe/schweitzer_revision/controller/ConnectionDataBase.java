f74585d1df5a6573daa05a393701f2785d5a8eaf
jhonyagus <jhonyagus@hotmail.com>
2019-07-17 22:18:25 -0500

    Connection DataBase

---
+++ 06.Code/SchweitzerSystem_2.0/src/ec/edu/espe/schweitzer_revision/controller/ConnectionDataBase.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.schweitzer_revision.controller;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jhony Naranjo
+ */
+public class ConnectionDataBase {
+ private DB db;
+ private DBCollection collection;
+ 
+    public ConnectionDataBase() {
+     try {
+         Mongo mongo = new Mongo("localhost",27017);
+         db = mongo.getDB("SchweitzerSystem");
+     } catch (UnknownHostException ex) {
+         Logger.getLogger(ConnectionDataBase.class.getName()).log(Level.SEVERE, null, ex);
+     }      
+    }
+
+    public DB getDb() {
+        return db;
+    }
+
+    public void setDb(DB db) {
+        this.db = db;
+    }
+
+    public DBCollection getCollection() {
+        return collection;
+    }
+
+    public void setCollection(DBCollection collection) {
+        this.collection = collection;
+    }
+    
+   
+}
